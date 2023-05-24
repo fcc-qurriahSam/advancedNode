45744d54caed83f85eb004b642a34af38f68c7c5
qurriahSam <mugirethe1@gmail.com>
2023-05-24 16:03:58 +0300

    feat: hash password with bcrypt

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -58,7 +59,7 @@ myDB(async (client) => {
           console.log(`User ${username} attempted to log in`);
           if (err) return done(err);
           if (!user) return done(null, false);
-          if (password !== user.password) return done(null, false);
+          if (!bcrypt.compareSync(password, user.password)) return done(null, false);
           return done(null, user);
         });
       })
@@ -96,9 +97,10 @@ myDB(async (client) => {
           res.redirect('/');
         } else {
           try {
+            const hashPass = bcrypt.hashSync(req.body.password, 12);
             const registerUser = await myDataBase.insertOne({
               username: req.body.username,
-              password: req.body.password,
+              password: hashPass,
             });
             next(null, registerUser.ops[0]);
           } catch (error) {
