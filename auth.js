a5744d85ff18e39c2a4642de84efb9b127228ba9
qurriahSam <mugirethe1@gmail.com>
2023-05-24 20:33:05 +0300

    feat: setup github authentication

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const GithubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
 const { ObjectID } = require('mongodb');
 
@@ -13,6 +15,43 @@ module.exports = (app, myDataBase) => {
       done(null, doc);
     });
   });
+
+  passport.use(
+    new GithubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'http://127.0.0.1:8080/auth/github/callback',
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || 'John Doe',
+              photo: profile.photos[0].value || '',
+              email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
+              created_on: new Date(),
+              provider: profile.provider || '',
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          }
+        );
+      }
+    )
+  );
 
   passport.use(
     new LocalStrategy((username, password, done) => {
