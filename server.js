4d418a7310aebe51c315139c1f32930d2f413af8
qurriahSam <mugirethe1@gmail.com>
2023-05-25 15:55:57 +0300

    feat: announce new users

--- server.js
+++ server.js
@@ -60,13 +60,13 @@ myDB(async (client) => {
 
     io.on('connection', (socket) => {
       ++currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', { username: socket.request.user.username, currentUsers, connected: true });
       console.log('A user has connected');
 
       socket.on('disconnect', () => {
         console.log('A user has disconnected');
         --currentUsers;
-        io.emit('user count', currentUsers);
+        io.emit('user', { username: socket.request.user.username, currentUsers, connected: false });
       });
     });
   } catch (error) {
